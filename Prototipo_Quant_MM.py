0fccf71f64d58d5448d6f7ac5cdf115a3717a078
Diogo Raucci <86979445+diogoraucci@users.noreply.github.com>
2023-07-31 14:01:13 -0300

    Atualização de arquivo

--- Prototipo_Quant_MM.py
+++ Prototipo_Quant_MM.py
@@ -768,7 +768,7 @@ with container:
 
             # Seletor Média Móvel
             MM = tuple(df_dataset['MM'][df_dataset.index == select_tickers])
-            mm_limit = int(len(MM) )
+            mm_limit = int(len(MM) > 20)
             if mm_limit < 1:
                 MM = MM[-1:]
             else:
