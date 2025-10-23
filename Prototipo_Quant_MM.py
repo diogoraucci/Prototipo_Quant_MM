6f27b481ad7479b9e01a697f613e2a0a3de33eae
Diogo Raucci <86979445+diogoraucci@users.noreply.github.com>
2025-10-23 11:23:41 -0300

    Atualização de arquivo

--- Prototipo_Quant_MM.py
+++ Prototipo_Quant_MM.py
@@ -63,7 +63,7 @@ def carregar_descricao():
     response = requests.get(url)
     conteudo_excel = response.content
     # Lê o conteúdo baixado como um arquivo Excel usando BytesIO
-    df_descricao = pd.read_excel(BytesIO(conteudo_excel), sheet_name='descricaoBR', index_col=1, engine='openpyxl')
+    df_descricao = pd.read_excel(BytesIO(conteudo_excel), sheet_name='descricaoBR', index_col=0, engine='openpyxl')
     return df_descricao
 
 @st.cache_data()
@@ -576,4 +576,423 @@ def main_arrojado(select_MM, select_tickers):
     fig.add_trace(go.Scatter(x=ts[ts['abaixo_3std']].index, y=ts.loc[ts['abaixo_3std'], 'Resíduos'], mode='markers',
                              marker=dict(color=dotColor3, size=15)))
 
-    fig.update_layout(title=f'Gráfico
+    fig.update_layout(title=f'Gráfico Normalizado {select_tickers} Média Móvel de {select_MM} períodos')
+    fig.update_layout(showlegend=False)  # Remove as legendas
+    # Remover o eixo Y
+    fig.update_layout(yaxis=dict(showticklabels=False, showgrid=False))
+    fig.update_layout(yaxis=dict(showline=False, zeroline=False))
+    # Define a legenda na parte interna do gráfico
+    fig.update_layout(legend=dict(x=0, y=1.1, orientation='h', traceorder='normal'), autosize=True, height=400)
+
+    # Exibir 8 datas no eixo x
+    num_dates = 5
+    tick_values = ts.index[::max(1, len(ts.index) // num_dates)]
+    tick_values = [ts.index[0]] + tick_values.tolist() + [
+        ts.index[-1]]  # Adiciona o primeiro e o último valor do DataFrame
+    fig.update_layout(xaxis=dict(tickmode='array', tickvals=tick_values, tickangle=0))
+
+    # Exibição do gráfico no Streamlit com largura de 100% e altura igual a 50% da largura
+    st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+
+def chart_vol(select_tickers):
+    # Coleta das cotações
+    df_chart = pd.DataFrame(df_cotacoes[select_tickers]).copy()
+
+    # Cálculo da volatilidade em janela móvel
+    df_chart['Vol'] = df_chart[select_tickers].pct_change().rolling(window=21).std() * np.sqrt(252)
+    df_chart.dropna(inplace=True)
+
+    df_chart['Baixa'] = np.percentile(df_chart['Vol'], 20)
+    df_chart['Alta'] = np.percentile(df_chart['Vol'], 80)
+    df_chart['Mean'] = df_chart['Vol'].mean()
+
+    Mean = df_chart['Vol'].mean()
+    std = df_chart['Vol'].std()
+
+    df_chart['+1Desv'] = Mean + (std * 1)
+    df_chart['-1Desv'] = Mean + (std * -1)
+    df_chart['+2Desv'] = Mean + (std * 2)
+    df_chart['-2Desv'] = Mean + (std * -2)
+
+    if df_chart['Vol'].iloc[-1] < df_chart['-1Desv'].iloc[-1] and df_chart['Vol'].iloc[-1] > df_chart['-2Desv'].iloc[-1]:
+        classVol = 'BAIXA'
+    elif df_chart['Vol'].iloc[-1] < df_chart['-2Desv'].iloc[-1]:
+        classVol = 'MUITO BAIXA'
+    elif df_chart['Vol'].iloc[-1] > df_chart['+1Desv'].iloc[-1] and df_chart['Vol'].iloc[-1] < df_chart['+2Desv'].iloc[-1]:
+        classVol = 'ALTA'
+    elif df_chart['Vol'].iloc[-1] > df_chart['+2Desv'].iloc[-1]:
+        classVol = 'MUITO ALTA'
+    else:
+        classVol = 'NEUTRA'
+
+    # Grafico de Volatilidade
+    # Plot do gráfico
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=df_chart.index, y=df_chart['Vol'], name=select_tickers, mode='lines',
+                             line=dict(color=LineVol, width=1)))
+    # fig.add_trace(go.Scatter(x=df_chart.index, y=df_chart['Mean'], name='Vol Média', mode='lines'))
+    fig.add_trace(go.Scatter(x=df_chart.index, y=df_chart['-1Desv'], name='-1', mode='lines',
+                             line=dict(color=horizontalLineColor, width=0.5, dash='dot')))
+    fig.add_trace(go.Scatter(x=df_chart.index, y=df_chart['+1Desv'], name='+1', mode='lines',
+                             line=dict(color=horizontalLineColor, width=0.5, dash='dot')))
+    fig.add_trace(go.Scatter(x=df_chart.index, y=df_chart['-2Desv'], name='-2', mode='lines',
+                             line=dict(color=horizontalLineColor, width=0.5, dash='dot')))
+    fig.add_trace(go.Scatter(x=df_chart.index, y=df_chart['+2Desv'], name='+2', mode='lines',
+                             line=dict(color=horizontalLineColor, width=0.5, dash='dot')))
+    fig.update_layout(title=f'Volatildiade Atual: {classVol}')
+
+    # Incluir um círculo branco no último valor de 'df_chart['Vol']'
+    ultimo_valor_vol = df_chart['Vol'].iloc[-1]
+    fig.add_trace(
+        go.Scatter(x=[df_chart.index[-1]], y=[ultimo_valor_vol], mode='markers', marker=dict(color='white', size=10)))
+
+    # Define a legenda na parte interna do gráfico
+    fig.update_layout(legend=dict(x=0, y=1.0, orientation='h', traceorder='normal'), showlegend=False, autosize=True,
+                      height=400)
+
+    # Exibir 8 datas no eixo x
+    num_dates = 3
+    tick_values = df_chart.index[::max(1, len(df_chart.index) // num_dates)]
+    tick_values = [df_chart.index[0]] + tick_values.tolist() + [
+        df_chart.index[-1]]  # Adiciona o primeiro e o último valor do DataFrame
+    fig.update_layout(xaxis=dict(tickmode='array', tickvals=tick_values, tickangle=0))
+    # Remover o eixo Y
+    fig.update_layout(yaxis=dict(showticklabels=False, showgrid=False))
+    fig.update_layout(yaxis=dict(showline=False, zeroline=False))
+
+    # Exibição do gráfico no Streamlit com largura de 100% e altura igual a 50% da largura
+    st.plotly_chart(fig, use_container_width=True)
+
+# Coletar Dados ==========================================================
+df_descricao = carregar_descricao()
+df_rankFM = carregar_rankFM()
+
+# Verificador Cache
+url1 = "https://raw.githubusercontent.com/diogoraucci/Prototipo_Quant_MM/main/dataControl.csv"
+# Faz o download do conteúdo do arquivo
+response = requests.get(url1)
+conteudo_csv = response.content
+# Lê o conteúdo baixado como um DataFrame do pandas
+dataControl = pd.read_csv(BytesIO(conteudo_csv))#, index_col=0)
+teteData = pd.DataFrame([[dataControl.iat[0, 0]]], columns=['0'])
+#teteData = pd.DataFrame({'Data': [teteData]})
+#st.dataframe(teteData)
+
+
+
+
+url2 = "https://raw.githubusercontent.com/diogoraucci/Prototipo_Quant_MM/main/DF_FormulaMagica.xlsx"
+# Faz o download do conteúdo do arquivo
+response = requests.get(url2)
+conteudo_excel = response.content
+# Lê o conteúdo baixado como um arquivo Excel usando BytesIO
+df_cotacoes2 = pd.read_excel(BytesIO(conteudo_excel), sheet_name='cotacoesBR', index_col=0, engine='openpyxl')
+df_cotacoes2 = pd.DataFrame(df_cotacoes2)
+#df_cotacoes2 = df_cotacoes2.index[-1]
+
+df_cotacoes2 = pd.DataFrame([[df_cotacoes2.index[-1]]])
+#chart_vol carregar_resultado carregar_rankFM carregar_descricao
+
+teteData2 = pd.DataFrame([[dataControl.iat[0, 0]]], columns=['0'])
+#teteData = pd.DataFrame({'Data': [teteData]})
+#st.dataframe(teteData2)
+
+#df_cotacoes.index[-1]
+teste2 = pd.DataFrame(['Diferentes'])  # ================================
+teste1 = pd.DataFrame(['Iguais'])  # ================================
+if teteData2.equals(teteData):
+    # Os DataFrames são iguais
+    #st.dataframe(teste1)
+    xx=0
+
+else:
+    # Os DataFrames são diferentes
+    #st.dataframe(teste2)  # ================================
+    st.cache_data.clear()
+
+#st.dataframe(teteData)
+#st.dataframe(teteData2)
+#st.dataframe(df_cotacoes2)
+
+
+
+# CORPO DA PÁGINA # =========================================================================
+st.title('Análise Fundamentalista e Quantitativa de Ações')
+
+# Menu Lateral
+add_selectbox = st.sidebar.selectbox(
+    "!!!EM DESENVOLVIMENTO!!!",
+    ("Ações B3", "Opções  !!EM BREVE!! ", "Long&Short Forex  !!EM BREVE!! ", "Crypto  !!EM BREVE!! ")
+)
+
+with st.sidebar:
+    add_radio = st.radio(
+        "Vencimento de Opções !! EM DESENVOLVIMENTO !!",
+        ("Data de Montagem 01/01/2222", "Data do Exercício 01/01/2222")
+    )
+
+# Criando o container com três partes
+container = st.container()
+
+# Menu ==================================
+with container:
+    fail = False
+    col0,col1, col2, col3, col4 = st.columns(5)
+
+    df_resultadosAnual, df_resultadosTrim = carregar_resultado()
+    df_cotacoes = carregar_cotacoes()
+
+
+    # Verificardor Limpeza de Cache
+    dataControl_cache = pd.DataFrame([df_cotacoes.index[-1]])
+    # Verificar se a última data do df_cotacoes é maior que a primeira data do dataControl_cache
+    if df_cotacoes.index[-1] > dataControl_cache.iat[0, 0]:
+        # Se for maior, limpar a memória cache do DataFrame df_cotacoes
+        st.experimental_memo.clear(df_cotacoes)
+        st.experimental_memo.clear(df_dataset)
+        st.cache.clear()
+    else:
+        # Caso contrário, não fazer nada
+        pass
+
+    # Seletor Sinal de Entrada
+    select_sinalEntrada = col0.selectbox("Modo de Visualização", ['Analizar Ativos', 'Rastreador de Entrada'])
+
+    # Seletor Perfil de Risco
+    select_PerfilRisco = col1.selectbox("Perfil de Risco", ['Conservador', 'Moderado', 'Arrojado'])
+    if select_PerfilRisco == 'Conservador':
+        filtroScore = 9
+        df_dataset = carregar_dataset()
+        df_dataset = df_dataset[(df_dataset['ADF'] >= 0.95)]
+        df_dataset = df_dataset[(df_dataset['Score_AnoTrim'] >= filtroScore) & (df_dataset['MM'] >= 50) & (df_dataset['Angle_1y'] > 0)]
+
+
+        if select_PerfilRisco == 'Conservador' and select_sinalEntrada == 'Rastreador de Entrada':
+            df_dataset = df_dataset[(df_dataset['Sinal'] == 'Rastreador de Entrada') & (df_dataset['STD'] <= -1)]
+
+            # Seletor Ações
+            tickers = list(df_dataset.index.drop_duplicates())
+            select_tickers = col2.selectbox('Selecione um Ticker', sorted(tickers))
+            #df_cotacoes = carregar_cotacoes()
+
+            # Seletor Média Móvel
+            MM = tuple(df_dataset['MM'][df_dataset.index == select_tickers])
+            mm_limit = int(len(MM))
+            if mm_limit < 1:
+                MM = MM[-1:]
+            else:
+                MM = MM[len(MM) - mm_limit:]
+
+            select_MM = col3.selectbox('Média Móvel', MM)
+
+        elif select_PerfilRisco == 'Conservador' and select_sinalEntrada == 'Analizar Ativos':
+
+            # Seletor Ações
+            tickers = list(df_dataset.index.drop_duplicates())
+            select_tickers = col2.selectbox('Selecione um Ticker', sorted(tickers))
+            #df_cotacoes = carregar_cotacoes()
+
+            # Seletor Média Móvel
+            MM = tuple(df_dataset['MM'][df_dataset.index == select_tickers])
+            mm_limit = int(len(MM))
+            if mm_limit < 1:
+                MM = MM[-1:]
+            else:
+                MM = MM[len(MM) - mm_limit:]
+            select_MM = col3.selectbox('Média Móvel', MM)
+
+    elif select_PerfilRisco == 'Moderado':
+        filtroScore = 8
+        #df_dataset = carregar_dataset()
+        #df_dataset = df_dataset[(df_dataset['ADF'] >= 0.95)]
+        #df_dataset = df_dataset[(df_dataset['Score_AnoTrim'] >= filtroScore) & (df_dataset['MM'] >= 50) & (df_dataset['Angle_1y'] > 0)]
+
+        if select_PerfilRisco == 'Moderado' and select_sinalEntrada == 'Rastreador de Entrada':
+            df_dataset = carregar_dataset()
+            df_dataset = df_dataset[(df_dataset['ADF'] >= 0.95) & (df_dataset['Score_AnoTrim'] >= filtroScore) & (df_dataset['MM'] >= 50) & (df_dataset['Angle_1y'] > 0) & (df_dataset['Sinal'] == 'Yes') & ((df_dataset['STD'] <= -1) | (df_dataset['STD'] >= 2))]
+
+            #df_dataset = df_dataset[(df_dataset['Sinal'] == 'Yes') | (df_dataset['STD'] != 1)]
+
+            # Seletor Ações
+            tickers = list(df_dataset.index.drop_duplicates())
+            select_tickers = col2.selectbox('Selecione um Ticker', sorted(tickers))
+            #df_cotacoes = carregar_cotacoes()
+
+            # Seletor Média Móvel
+            MM = tuple(df_dataset['MM'][df_dataset.index == select_tickers])
+            mm_limit = int(len(MM))
+            if mm_limit < 1:
+                MM = MM[-1:]
+            else:
+                MM = MM[len(MM) - mm_limit:]
+
+            select_MM = col3.selectbox('Média Móvel', MM)
+
+        elif select_PerfilRisco == 'Moderado' and select_sinalEntrada == 'Analizar Ativos':
+
+            # Seletor Ações
+            df_dataset = carregar_dataset()
+            tickers = list(df_dataset.index.drop_duplicates())
+            select_tickers = col2.selectbox('Selecione um Ticker', sorted(tickers))
+            #df_cotacoes = carregar_cotacoes()
+
+            # Seletor Média Móvel
+            MM = tuple(df_dataset['MM'][df_dataset.index == select_tickers])
+            mm_limit = int(len(MM))
+            if mm_limit < 1:
+                MM = MM[-1:]
+            else:
+                MM = MM[len(MM) - mm_limit:]
+            select_MM = col3.selectbox('Média Móvel', MM)
+
+    elif select_PerfilRisco == 'Arrojado':
+        # Seletor Ranck Ações
+        select_RankAcoes = col4.selectbox('Score das Ações', ('Todas as Ações', '5 Pts', '6 Pts', '7 Pts', '8 Pts', '9 Pts', '10 Pts'))
+        if select_RankAcoes == 'Todas as Ações':
+            filtroScore = 0
+        elif select_RankAcoes == '5 Pts':
+            filtroScore = 5
+        elif select_RankAcoes == '6 Pts':
+            filtroScore = 6
+        elif select_RankAcoes == '7 Pts':
+            filtroScore = 7
+        elif select_RankAcoes == '8 Pts':
+            filtroScore = 8
+        elif select_RankAcoes == '9 Pts':
+            filtroScore = 9
+        elif select_RankAcoes == '10 Pts':
+            filtroScore = 10
+
+        df_dataset = carregar_dataset()
+        df_dataset = df_dataset[(df_dataset['ADF'] >= 0.90)]
+        df_dataset = df_dataset[(df_dataset['Score_AnoTrim'] >= filtroScore)]
+
+        if select_PerfilRisco == 'Arrojado' and select_sinalEntrada == 'Rastreador de Entrada':
+            df_dataset = df_dataset[(df_dataset['Sinal'] == 'Yes')]
+
+            # Seletor Ações
+            tickers = list(df_dataset.index.drop_duplicates())
+            select_tickers = col2.selectbox('Selecione um Ticker', sorted(tickers))
+            #df_cotacoes = carregar_cotacoes()
+
+            # Seletor Média Móvel
+            MM = tuple(df_dataset['MM'][df_dataset.index == select_tickers])
+            select_MM = col3.selectbox('Média Móvel', MM)
+
+        elif select_PerfilRisco == 'Arrojado' and select_sinalEntrada == 'Analizar Ativos':
+
+            # Seletor Ações
+            tickers = list(df_dataset.index.drop_duplicates())
+            select_tickers = col2.selectbox('Selecione um Ticker', sorted(tickers))
+
+            # Seletor Média Móvel
+            MM = tuple(df_dataset['MM'][df_dataset.index == select_tickers])
+            select_MM = col3.selectbox('Média Móvel', MM)
+
+ # ----------------------------------------------------------------------------------
+# Segunda parte com gráficos de linhas e de barras horizontais
+# Plota o gráfico Trimestral e Anual de barras ==============================================
+res_trim = df_resultadosTrim[df_resultadosTrim['TICKER'] == select_tickers ]
+res_anual = df_resultadosAnual[df_resultadosAnual['TICKER'] == select_tickers ]
+
+with container:
+    col1, col2 = st.columns([6, 2])
+
+    # Gráfico de linhas na coluna 1
+    with col1:
+        if __name__ == '__main__':
+            #main(select_MM, select_tickers)
+            try:
+                if select_PerfilRisco == 'Conservador':
+                   main_conservador(select_MM, select_tickers)
+
+                elif select_PerfilRisco == 'Moderado':
+                    main_moderado(select_MM, select_tickers)
+
+                elif select_PerfilRisco == 'Arrojado':
+                    main_arrojado(select_MM, select_tickers)
+
+            except:
+                st.markdown(f'No momento, não há nenhuma oportunidade no Perfil De Risco {select_PerfilRisco}')
+
+with col2:
+    # --- Descrição da Empresa (moved to top for prominence) ---
+    with st.expander(f"Descrição da Empresa ({select_tickers})", expanded=True):
+        try:
+            descricao = df_descricao.at[select_tickers, 'Descricao']
+            st.markdown(descricao)
+        except Exception as e:
+            st.markdown(f"**Erro**: Descrição indisponível para {select_tickers}: {e}")
+            print(f"Erro ao carregar descrição para {select_tickers}: {e}")
+
+    # --- Preparação segura dos dados (Trimestral) ---
+    try:
+        res_trim['Data'] = pd.to_datetime(res_trim['Data'], errors='coerce')
+        res_trim = res_trim.dropna(subset=['Data']).copy()
+        res_trim['DataStr'] = res_trim['Data'].dt.strftime('%Y-%m-%d')
+        res_trim.rename(columns={'Net Income': 'NetIncome'}, inplace=True)
+        res_trim['NetIncome'] = pd.to_numeric(res_trim['NetIncome'], errors='coerce')
+        res_trim = res_trim.dropna(subset=['NetIncome']).copy()
+        
+        # --- Gráfico Trimestral ---
+        chartwidth = 300  # Reduced from 800
+        chartheight = 200  # Adjusted for proportionality
+        chart_trim = (
+            alt.Chart(res_trim)
+            .mark_bar()
+            .encode(
+                y=alt.Y('DataStr:O', title='Data', sort=alt.EncodingSortField(field='Data', order='ascending')),
+                x=alt.X('NetIncome:Q', title='Lucro Líquido (R$)'),
+                color=alt.condition(alt.datum.NetIncome > 0, alt.value(colorUp), alt.value(colorDown)),
+                facet=alt.Facet('TICKER:N', title='')
+            )
+            .properties(
+                title='Resultado Trimestral',
+                width=chartwidth,
+                height=chartheight
+            )
+            .configure_axis(labelFontSize=12, titleFontSize=14)
+        )
+        st.altair_chart(chart_trim)  # Removed use_container_width=True
+    except Exception as e:
+        st.markdown(f"**Erro**: Falha ao exibir gráfico trimestral para {select_tickers}: {e}")
+        print(f"Erro no gráfico trimestral: {e}")
+
+    # --- Preparação segura dos dados (Anual) ---
+    try:
+        res_anual['Data'] = pd.to_datetime(res_anual['Data'], errors='coerce')
+        res_anual = res_anual.dropna(subset=['Data']).copy()
+        res_anual['DataStr'] = res_anual['Data'].dt.strftime('%Y-%m-%d')
+        res_anual.rename(columns={'Net Income': 'NetIncome'}, inplace=True)
+        res_anual['NetIncome'] = pd.to_numeric(res_anual['NetIncome'], errors='coerce')
+        res_anual = res_anual.dropna(subset=['NetIncome']).copy()
+        
+        # --- Gráfico Anual ---
+        chart_anual = (
+            alt.Chart(res_anual)
+            .mark_bar()
+            .encode(
+                y=alt.Y('DataStr:O', title='Data', sort=alt.EncodingSortField(field='Data', order='ascending')),
+                x=alt.X('NetIncome:Q', title='Lucro Líquido (R$)'),
+                color=alt.condition(alt.datum.NetIncome > 0, alt.value(colorUp), alt.value(colorDown)),
+                facet=alt.Facet('TICKER:N', title='')
+            )
+            .properties(
+                title='Resultado Anual',
+                width=chartwidth,
+                height=chartheight
+            )
+            .configure_axis(labelFontSize=14, titleFontSize=16)
+        )
+        st.altair_chart(chart_anual)  # Removed use_container_width=True
+    except Exception as e:
+        st.markdown(f"**Erro**: Falha ao exibir gráfico anual para {select_tickers}: {e}")
+        print(f"Erro no gráfico anual: {e}")
+
+    # --- Gráfico de Volatilidade ---
+    try:
+        chart_vol(select_tickers)
+    except Exception as e:
+        st.markdown(f"**Erro**: Falha ao exibir gráfico de volatilidade para {select_tickers}: {e}")
+        print(f"Erro no gráfico de volatilidade: {e}")
+
